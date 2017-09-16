6892773284041a98caa44de52655441d3b9fe041
SpadeXavier <2017.vkrishnan@gmail.com>
2017-09-16 16:51:48 -0500

    Added instruction

--- setup.py
+++ setup.py
@@ -1,5 +1,7 @@
 from cx_Freeze import setup, Executable 
 
+# run python setup.py build in the directory with this file, and the resulting
+# build directory will have the wutp executable 
 setup(name='wutp',
       version='0.1',
       description='Wake Up to Politics',
