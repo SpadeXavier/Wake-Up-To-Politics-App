5a784a25eeaf8c2045a6b728496e1e7cab0d00a2
SpadeXavier <2017.vkrishnan@gmail.com>
2017-09-16 18:32:16 -0500

    Added tkinter support

--- setup.py
+++ setup.py
@@ -1,12 +1,20 @@
-from cx_Freeze import setup, Executable 
+import cx_Freeze
 
 # run python setup.py build in the directory with this file, and the resulting
 # build directory will have the wutp executable 
-# Note: may have to manually move images directory into builds folder 
-setup(name='wutp',
+# Note: might have to manually move images folder to build exe directory
+
+base = None
+
+executables = [cx_Freeze.Executable("wutp_app.py", base=base)] 
+
+cx_Freeze.setup(name='wutp',
+      options={"build_exe": {"packages":["tkinter"],
+      "include_files":["images/previous.png", "images/next.png"]}},
       version='0.1',
       description='Wake Up to Politics',
-      executables = [Executable("wutp_app.py")])
+      executables=executables
+      )
 
 
 
