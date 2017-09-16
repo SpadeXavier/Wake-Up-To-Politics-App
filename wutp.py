6637d9c17caff2d76941451b02e590f7be5ba45b
SpadeXavier <2017.vkrishnan@gmail.com>
2017-09-16 12:53:47 -0500

    Added better parsing for headings and points

--- wutp.py
+++ wutp.py
@@ -2,6 +2,7 @@
 
 from urllib.request import urlopen as uReq 
 from bs4 import BeautifulSoup as soup
+from collections import OrderedDict
 
 class Wutp():
 
@@ -16,63 +17,83 @@ class Wutp():
         self.parsed = soup(html, "html.parser") 
 
     def convert(self, string):
-        return string.encode('ascii', 'ignore').decode('utf-8') 
+        string = string.replace('\n', '').replace(u'\xa0', ' ')
+        string = string.encode('ascii', 'ignore').decode('utf-8')
+        return string
 
+    def traverse(self):
+        """ traverses a page and returns an ordered dict of headings as keys
+        and corresponding points as values """  
+
+        content = self.parsed.findAll("table", {"class": 'mcnTextContentContainer'})
+        
+        news = OrderedDict() 
+        heading_list = []   # keeps tracks of added headings
+        point_list = []     # keeps track of added points
+        points = False      # keeps track of if a point or a heading
+        current_heading = ''
+
+        for index,c in enumerate(content):
+            if points:
+                all_points = c.findAll('li')
+
+                # -- if there are lists use the text of lists
+                if len(all_points) > 0:
+                    for each_point in all_points:
+                        nested_points = each_point.findAll('li') 
+                        # -- if there are nested lists get only the first span
+                        # tag because the loop will grab the texts of the
+                        # other lists later on 
+                        if len(nested_points) > 0:
+                            first_tag = each_point.find('span') 
+                            point_list.append(first_tag.text)
+                            news[current_heading].append(self.convert(first_tag.text))
+                        # -- if no nested list just grab the text of the list 
+                        else:
+                            point_list.append(each_point.text) 
+                            news[current_heading].append(self.convert(each_point.text))
+
+                # -- if no lists but still after a heading, then just grab the text 
+                else:
+                    if c.text not in point_list:
+                        point_list.append(c.text) 
+                        news[current_heading].append(self.convert(c.text))
+
+            # -- if it's a heading add it under headings 
+            if c['cellpadding'] == '18':
+                if c.text not in heading_list:
+                    heading_list.append(c.text)
+                    current_heading = self.convert(c.text)
+                    news[current_heading] = [] 
+                # -- next iteration will be points
+                points = True        
+          
+            # -- need try and except for index out of range error 
+            try:
+                if content[index+1]['cellpadding'] == '18':
+                    # -- next iteration will be a heading
+                    points = False
+            except:
+                pass
+       
+        # -- remove last item which is copyright footer 
+        news.popitem()
+
+        self.news = news
+        
+        return news
 
     def get_headings(self):
         """ returns an array of the headings in the page """ 
-        headings = self.parsed.findAll("table", {"class":"mcnBoxedTextContentContainer"})
-        heading_array = []
-        for heading in headings:
-            try:
-                heading_text = heading.find('strong').text
-                heading_text = self.convert(heading_text)
-                heading_array.append(heading_text)
-            except:
-                pass
-        return heading_array 
+        return [key for key in self.news] 
 
     def get_points(self):
         ''' returns a two dimensional array with points of a heading at each
         index''' 
 
-        points_array = []  
-        curr_points = []
+        return [v for key,v in self.news.items()]  
 
-        bodies = self.parsed.findAll('td', {"class": 'mcnTextContent'})
-
-        for body in bodies:
-            points = body.findAll('li')
-
-            # skip if no lists 
-            if len(points) == 0:
-                continue 
-
-            for point in points:
-                point_text = point.text 
-                point_text = point_text.replace(u'\xa0', ' ')
-                point_text = self.convert(point_text)
-                curr_points.append(point_text)            
-
-            points_array.append(curr_points)
-            curr_points = []
-
-        return points_array
-
-    def get_content(self):
-        """ returns a dictionary with headings as key and the corresponding
-        points as an array value """
-        
-        content = {}
-
-        headings = self.get_headings() 
-        points = self.get_points() 
-
-        for heading, point_container in zip(headings,points):
-            content[heading] = point_container
-
-        return content
-
+    
     @staticmethod
     def get_urls():
         """ returns a dictionary of issues with url title as key and url target
@@ -144,18 +165,20 @@ class Wutp():
 
 
 if __name__ == '__main__':
-    url = 'http://mailchi.mp/wakeuptopolitics/091317?e=58381c74c0'
+    url = 'http://us3.campaign-archive2.com/?u=4946817b18454973fb1cd7ecc&id=091f19cbd4'
     wutp = Wutp(url)
+    wutp.traverse()  
+
     print('------------------ HEADINGS ----------------------')
     print(wutp.get_headings()) 
-    # print('------------------ POINTS ------------------------') 
-    # print(wutp.get_points())
-    print('------------------- CONTENT ----------------------') 
-    print(wutp.get_content())
-    print('------------------- URLS -------------------------') 
-    print(Wutp.get_urls())
-    print('-------------------- DATES ------------------------') 
-    print(Wutp.get_dates()) 
+    print('------------------ POINTS ------------------------') 
+    print(wutp.get_points())
+    # print('------------------- CONTENT ----------------------') 
+    # print(wutp.get_content())
+    # print('------------------- URLS -------------------------') 
+    # print(Wutp.get_urls())
+    # print('-------------------- DATES ------------------------') 
+    # print(Wutp.get_dates()) 
 
 
 
