7b1bff4b44211a72681fb619e3c265bff6f804fa
SpadeXavier <2017.vkrishnan@gmail.com>
2017-09-16 16:53:00 -0500

    refined instructions

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@ from cx_Freeze import setup, Executable
 
 # run python setup.py build in the directory with this file, and the resulting
 # build directory will have the wutp executable 
+# Note: may have to manually move images directory into builds folder 
 setup(name='wutp',
       version='0.1',
       description='Wake Up to Politics',
